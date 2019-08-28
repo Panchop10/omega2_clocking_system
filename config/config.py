52a5b45333bbb80b7e94014a91b83a6274b00be7
Panchop <folivares@arenasoft.cl>
2019-08-28 12:32:19 +0100

    path directory in config file

--- config/config.py
+++ config/config.py
@@ -2,11 +2,11 @@
 
 #Python Modules
 from enum import Enum
-from pathlib import Path
+import os
 
 class Config(Enum):
     #CURRENT DIRECTORY
-    PROJECT_DIR = str(Path().absolute())
+    PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
     #USER SETTINGS
     DELAY_SECONDS = 10 #DELAY FOR CLOCKING AGAIN IN SECONDS (AVOID DUPLICATE)
@@ -14,8 +14,8 @@ class Config(Enum):
     ID_STOP_SYSTEM = "b5c6e7bb" #Card id to stop the system
 
     #DONT CHANGE
-    OK_IMG = PROJECT_DIR+"/img/image_ok.lcd" #Path for OK img
-    ERROR_IMG = PROJECT_DIR+"/img/image_error.lcd" #Path for error img
+    OK_IMG = PROJECT_DIR+"/../img/image_ok.lcd" #Path for OK img
+    ERROR_IMG = PROJECT_DIR+"/../img/image_error.lcd" #Path for error img
 
     #MYSQL CONFIGURATION
     HOST = "192.168.1.233"
@@ -25,5 +25,5 @@ class Config(Enum):
     DATABASE = "staff"
 
     #GOOGLE SPREADSHEET CONFIGURATION
-    KEY_FILE = PROJECT_DIR+"/google_spreadsheet/omega_ned_key.json"
+    KEY_FILE = PROJECT_DIR+"/../google_spreadsheet/omega_ned_key.json"
     SPREADSHEET_NAME = "timeclock_system"
